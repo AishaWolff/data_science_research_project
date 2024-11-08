34d9d857161d3eb5b6dc91f88eb0f1e4ef80702a
ravishende <ravi.sameer.shende@gmail.com>
2024-11-07 20:34:50 -0800

    added file for main file cleaning code to be written in

---
+++ preprocess_data.py
@@ -0,0 +1,8 @@
+"""
+This file is for cleaning, tidying, and analyzing the data. Here is where datasets
+will be imported from original_datasets, then fixed up and saved into cleaned_datasets.
+Here is where everyone will be writing code for their individual datasets.
+"""
+
+import pandas as pd
+from analysis import analyze
