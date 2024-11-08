3f88092f7dfbd252b7c2f38e488c7e27fb26a2bd
AishaWolff <a.wolff803@gmail.com>
2024-11-07 22:10:11 -0800

    Worked on tidy function, got all columns lowercase and correct years. Added orginal csv.

--- tidy.py
+++ tidy.py
@@ -16,3 +16,30 @@ Steps:
     Andorra | 2008 | 11.1
     Andorra | 2009 | 10.3
 """
+import pandas as pd
+
+def tidy (df):
+   #drop all rows not in desried time frame 
+   df = df[(df["TIME_PERIOD"] < 2019) & (df["TIME_PERIOD"] > 2000)]
+   #rename columns to be lowercase
+   all_columns = list(df.columns)
+   lower_columns = []
+   for column in all_columns:
+      lower_columns.append(column.lower())
+   rename_dict = dict(zip(all_columns,lower_columns))
+   df = df.rename(columns = rename_dict)
+   
+
+
+
+   
+
+
+    
+   return df
+
+#read in file from orginal_datasets folder
+df= pd.read_csv("original_datasets/healthcare_quality_and_outcomes.csv")
+#print(df.columns)
+df = tidy(df)
+#print(df.head)
