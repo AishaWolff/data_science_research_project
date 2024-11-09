ace853e2fa07183c91e85f4756d327bbebdc7e7f
ravishende <ravi.sameer.shende@gmail.com>
2024-11-09 12:57:37 -0800

    added function in tidy.py to drop columns in a dataframe that have some proportion of NA values

--- tidy.py
+++ tidy.py
@@ -18,40 +18,64 @@ Steps:
 """
 import pandas as pd
 
-def tidy (df,og_country_column = "Reference area",og_year_column="TIME_PERIOD",drop_columns = None):
 
-   #drop unneeded columns 
-   if drop_columns is None:
-      drop_columns = ["STRUCTURE","STRUCTURE_ID","STRUCTURE_NAME","ACTION","REF_AREA","FREQ","MEASURE","UNIT_MEASURE","FINANCING_SCHEME","FINANCING_SCHEME_REV","FUNCTION","MODE_PROVISION","PROVIDER","FACTOR_PROVISION","ASSET_TYPE","PRICE_BASE","Time period","Observation value","Base period","CURRENCY","UNIT_MULT","Decimals"]
-   df = df.drop(columns = drop_columns)
-   #rename unclear columns
-   df = df.rename(columns = {og_year_column:"year",og_country_column:"country"})
-   
-   #drop all rows not in desried time frame 
-   df = df[(df["year"] < 2019) & (df["year"] > 2000)]
-   
-   #rename columns to be lowercase and replace spaces with underscores
-   all_columns = list(df.columns)
-   lower_columns = []
-   for column in all_columns:
-      lower_columns.append(column.lower().replace(" ","_"))
-   rename_dict = dict(zip(all_columns,lower_columns))
-   df = df.rename(columns = rename_dict)
-   
-   #drop columns with all none values
-   df = df.dropna(axis = "columns",how = "all")
+def drop_cols_with_proportion_na(df: pd.DataFrame, proportion: float = 0.9) -> pd.DataFrame:
+    """
+    Drop any columns in the dataframe that are some proportion or more of NA values
+    Parameters:
+        df - dataframe to drop columns from
+        proportion - minimum proportion (between 0 and 1) of NA values in a column for that column to be dropped
+    Returns:
+        updated DataFrame
+    """
+    drop_cols = []
+    # collect all columns that have >= proportion NA values
+    for col in df.columns:
+        proportion_na = df[col].isna().mean()
+        if proportion_na >= proportion:
+            drop_cols.append(col)
+    # drop columns
+    df.drop(columns=drop_cols)
 
-   #drop duplicate columns 
 
-   #checking that function successfully changed column names
-   assert "country" in df.columns,"no country column found, orginal dataframe columns named differently than expected"
-   assert "year" in df.columns,"no year column found, orginal dataframe columns named differently than expected"
+def tidy(df, og_country_column="Reference area", og_year_column="TIME_PERIOD", drop_columns=None):
 
-   return df
+    # drop unneeded columns
+    if drop_columns is None:
+        drop_columns = ["STRUCTURE", "STRUCTURE_ID", "STRUCTURE_NAME", "ACTION", "REF_AREA", "FREQ", "MEASURE", "UNIT_MEASURE", "FINANCING_SCHEME", "FINANCING_SCHEME_REV", "FUNCTION",
+                        "MODE_PROVISION", "PROVIDER", "FACTOR_PROVISION", "ASSET_TYPE", "PRICE_BASE", "Time period", "Observation value", "Base period", "CURRENCY", "UNIT_MULT", "Decimals"]
+    df = df.drop(columns=drop_columns)
+    # rename unclear columns
+    df = df.rename(columns={og_year_column: "year",
+                   og_country_column: "country"})
 
-#read in file from orginal_datasets folder
-#for testing
-df = pd.read_csv("original_datasets/unfiltered_expenditure.csv")
-df = tidy(df)
-df.to_csv("testing_datasets/testing.csv",index = False)
-print(df.columns)
+    # drop all rows not in desried time frame
+    df = df[(df["year"] < 2019) & (df["year"] > 2000)]
+
+    # rename columns to be lowercase and replace spaces with underscores
+    all_columns = list(df.columns)
+    lower_columns = []
+    for column in all_columns:
+        lower_columns.append(column.lower().replace(" ", "_"))
+    rename_dict = dict(zip(all_columns, lower_columns))
+    df = df.rename(columns=rename_dict)
+
+    # drop columns with at least 90% NA values
+    drop_cols_with_proportion_na(df, 0.9)
+
+    # drop duplicate columns
+
+    # checking that function successfully changed column names
+    assert "country" in df.columns, "no country column found, orginal dataframe columns named differently than expected"
+    assert "year" in df.columns, "no year column found, orginal dataframe columns named differently than expected"
+
+    return df
+
+
+# read in file from orginal_datasets folder
+# for testing
+if __name__ == "__main__":
+    df = pd.read_csv("original_datasets/unfiltered_expenditure.csv")
+    df = tidy(df)
+    df.to_csv("testing_datasets/testing.csv", index=False)
+    print(df.columns)
