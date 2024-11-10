b655ad695ed1e3c9cc4c5ca6490179b1e2e860a2
ravishende <ravi.sameer.shende@gmail.com>
2024-11-10 09:42:59 -0800

    added medical technology availability dataset and tidied/analyzed

--- preprocess_data.py
+++ preprocess_data.py
@@ -7,3 +7,26 @@ Here is where everyone will be writing code for their individual datasets.
 import pandas as pd
 from analysis import analyze
 from tidy import tidy
+
+
+def medical_tech_availability():
+    read_file = "original_datasets/medical_tech_availability.csv"
+    df = pd.read_csv(read_file)
+    # most of these are duplicate short-version columns
+    unnecessary_cols = [
+        'STRUCTURE', 'STRUCTURE_ID', 'STRUCTURE_NAME', 'ACTION', 'MEASURE', 'UNIT_MEASURE',
+        'STATISTICAL_OPERATION', 'OWNERSHIP_TYPE', 'HEALTH_FUNCTION', 'CARE_TYPE',
+        'HEALTH_CARE_PROVIDER', 'Time period', 'Observation value', 'DECIMALS', 'Decimals',
+        'OBS_STATUS', 'OBS_STATUS2', 'OBS_STATUS3', 'UNIT_MULT', 'REF_YEAR_PRICE'
+    ]
+    data_cols_rename_dict = {
+        'OBS_VALUE': 'med_tech_availability_p_mil_ppl'
+    }
+    df_title = "medical_tech_availability"
+    df = tidy(df, df_title,
+              data_cols_rename_dict, drop_columns=unnecessary_cols)
+    analyze(df, df_title)
+    print("Dataframe:", df, sep="\n")
+
+
+medical_tech_availability()
