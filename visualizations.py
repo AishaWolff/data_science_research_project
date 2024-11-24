8e0628fc315179605545f7825b60c3efe9a4531d
ravishende <ravi.sameer.shende@gmail.com>
2024-11-24 13:14:14 -0800

    added plot for analyzing weird correlations

--- visualizations.py
+++ visualizations.py
@@ -26,6 +26,20 @@ def health_expenditure_p_capita_vs_health_expenditure_as_perc_gdp():
     sns.barplot(data=correlation_data, y='code', x='correlation', palette='viridis')
     plt.show()
 
+def analyze_weird_expenditure_correlations():
+    df = pd.read_csv('cleaned_datasets/inner_merged.csv')
+    correlation_data = df.groupby('code').apply(
+        lambda x: x['health_expenditure_as_percent_gdp'].corr(x['expenditure_per_capita'])
+    ).reset_index(name='correlation')
+    weird_countries = correlation_data[correlation_data['correlation'] < 0]['code'].to_list()
+    df_subset = df[df['code'].isin(weird_countries)]
+    
+    plt.rc('figure', figsize=(15, 8))
+    fig, (ax1, ax2) = plt.subplots(ncols=2, sharey=False)
+    sns.lineplot(data=df_subset, x='year', y='health_expenditure_as_percent_gdp', hue='code',palette='viridis', ax=ax1)
+    sns.lineplot(data=df_subset, x='year', y='expenditure_per_capita', hue='code', palette='viridis', ax=ax2)
+    plt.show()
+
 #avoidable deaths by country per year
 def death_by_country_over_time():
     df = pd.read_csv("cleaned_datasets/inner_merged.csv")
@@ -44,7 +58,8 @@ def hospital_stay_length_by_med_tech_avalibility_over_time():
 
 
 if __name__ == "__main__":
-    med_tech_availability_corr_with_expenditure()
-    health_expenditure_p_capita_vs_health_expenditure_as_perc_gdp()
-    death_by_country_over_time()
-    hospital_stay_length_by_med_tech_avalibility_over_time()
+    # med_tech_availability_corr_with_expenditure()
+    # health_expenditure_p_capita_vs_health_expenditure_as_perc_gdp()
+    # death_by_country_over_time()
+    # hospital_stay_length_by_med_tech_avalibility_over_time()
+    analyze_weird_expenditure_correlations()
